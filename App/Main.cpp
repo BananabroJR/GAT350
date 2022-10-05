e44902373d31a89039e95e997569c994aaff8a2e
BananabroJR <emiller@student.neumont.edu>
2022-10-04 19:19:41 -0600

    Can Make Triangles

--- App/Main.cpp
+++ App/Main.cpp
@@ -1,7 +1,7 @@
 #include "Engine.h" 
 #include <iostream> 
 
-int main()
+int main(int argc, char** argv)
 {
 	Skyers::InitializeMemory();
 
@@ -18,8 +18,22 @@ int main()
 		if (Skyers::g_inputSystem.GetKeyState(Skyers::key_escape) == Skyers::InputSystem::KeyState::Pressed) quit = true;
 
 		Skyers::g_renderer.BeginFrame();
+		
+		glBegin(GL_TRIANGLES);
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex2f(-0.5f, -0.5f);
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex2f(0.0f, 0.5f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(0.5f, -0.5f);
+
+		glEnd();
+
 		Skyers::g_renderer.EndFrame();
 	}
 
 	Skyers::Engine::Instance().Shutdown();
+
+	return 0;
 }
